9260439a6b02eebb56589c692047eaa6ece78899
yigittin <i.yigittin@hotmail.com>
2021-09-13 00:39:22 +0300

    Tutorial

---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question {
+  String questionText = '';
+  bool questionAnswer = false;
+
+  Question({required String q, required bool a}) {
+    questionText = q;
+    questionAnswer = a;
+  }
+}
